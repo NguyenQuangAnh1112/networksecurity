2ca1581bc8e70320150b51ed874fda8d7de912e8
muggle <qnh11122006@gmail.com>
2026-01-02 14:09:33 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_lst:List[str] = []
+    try:
+        with open("requirements.txt", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != "-e .":
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    
+    return requirement_lst
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Muggle",
+    author_email="qnh11122006@gmail.com",
+    packages=get_requirements(),
+    install_requires=get_requirements()
+)
